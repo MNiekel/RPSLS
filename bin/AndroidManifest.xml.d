df4a20fba80f513ad8498c3150d189d7813c35a2
MNiekel <maartje@niekel.net>
2012-11-30 07:32:38 +0100

    Clean up.

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Maartje\Development\Android\rpsls\bin\AndroidManifest.xml : \
-C:\Users\Maartje\Development\Android\rpsls\AndroidManifest.xml \
--- bin/RPSLS-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Maartje\Development\Android\rpsls\bin\RPSLS-debug-unaligned.apk : \
-C:\Users\Maartje\Development\Android\rpsls\bin\RPSLS.ap_ \
-C:\Users\Maartje\Development\Android\rpsls\bin\classes.dex \
-C:\Users\Maartje\AppData\Local\Android\android-sdk\tools\support\annotations.jar \
